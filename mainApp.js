7633bff1cd52254aa610f379ca9b162b2d56b21e
vadim khakham <vadim.khakham@gmail.com>
2016-07-21 17:23:31 +0300

    reordering

---
+++ mainApp.js
@@ -0,0 +1,35 @@
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var xScale = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .3);
+
+var yScale = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left")
+    .ticks(10, "%");
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+        return "<span style='color:red'>" + parseInt(d.values.votingPerc*100) + "%" + "</span><strong> :אחוזי הצבעה</strong>";
+    });
+
+svg.call(tip);
+
+generateVotingPercentage();
---
+++ voting-perc.js
@@ -0,0 +1,69 @@
+var generateVotingPercentage = function () {
+    d3.json("data-set-elections/electionAndEconomicData.json", function(error, data) {
+        if (error) throw error;
+
+        header_data = data.splice(0,3);
+        console.log(data);
+        console.log(header_data);
+
+        var votingDataByCluster = d3.nest()
+            .key(function (d) {return d.cluster;})
+            .rollup(function (cities) {
+                var totalVotesForCluster = d3.sum(cities, function(g) {return g.votes});
+                var votingPercForCluster = totalVotesForCluster / d3.sum(cities, function(g) {return g.eligible});
+                var rightAttributeForCluster = d3.sum(cities, function(g) {return g.right_attitude}) / totalVotesForCluster
+                return { votingPerc: votingPercForCluster, rightAttribute: rightAttributeForCluster };
+            })
+            .entries(data);
+        console.log(votingDataByCluster);
+        xScale.domain(d3.range(1, 11, 1));   // social classes [1,...,10]
+        yScale.domain([0, 1]);
+//        yScale.domain([0, d3.max(votingDataByCluster, function (d) { return d.values.votingPerc })]); // max percent is highest
+
+
+        var averageVotingPerc = d3.sum(data, function (d) { return d.votes}) / d3.sum(data, function(d) { return d.eligible});
+
+        console.log(averageVotingPerc);
+        var colorScale = d3.scale.linear()
+            .range(["#ffe6e6", "#990000"])
+            .domain(d3.extent(votingDataByCluster, function (d) { return d.values.rightAttribute }));
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Voting Percentage");
+
+        svg.append("line")
+            .attr("x1", 0)
+            .attr("y1", yScale(averageVotingPerc))
+            .attr("x2", width)
+            .attr("y2", yScale(averageVotingPerc))
+            .attr("style", 'stroke:red;stroke-width:2;stroke-dasharray: 10;');
+
+        svg.selectAll(".bar")
+            .data(votingDataByCluster)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return xScale(d.key); })
+            .attr("width", xScale.rangeBand())
+            .attr("y", function(d) { return yScale(d.values.votingPerc); })
+            .attr("height", function(d) { return height - yScale(d.values.votingPerc); })
+            .attr("style", function (d) {return "fill:" + colorScale(d.values.rightAttribute)})
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide)
+
+        ;
+    });
+};
+
