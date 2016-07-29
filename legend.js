e0c0ed12accdfb5c7b6925c471e9595e5e2bdd8f
gilad eini <giladeini@gmail.com>
2016-07-29 08:39:21 +0300

    legend clean up - CSS NEEDED

--- legend.js
+++ legend.js
@@ -1,41 +1,50 @@
 function legend() {
-    var legendLabels = ["ימין-", "שמאל-"];
 
+    /*rect(surrounding legend) attributes and style */
+    var rectTransfrom = "translate(" + (margin.left + width - 100 ) + "," + (margin.top - 10) + ")";
+    var rectBorderColor = "rgb(87, 114, 234)";
+    var rectBorderWidth = 2;
+    var rectOpacity = 0.0001;
+    var rectWidth = 95;
+    var rectHeight = 160;
+
+    /*legend attributes and style*/
+    var fontFamily = "Times New Roman";
+    var legendLabels = ["ימין-", "שמאל-"];
     var legendWidth = 20;
     var legendHeight = 140;
+    var legendTransfrom = "translate(" + (margin.left + width - 40) + "," + (margin.top) + ")";
 
     var rectAboveLegend = parent_svg
             .append("g")
             .append("rect")
-            .attr("id", "x")
-            .attr("transform", "translate(" + (margin.left + width - 100 ) + "," + (margin.top - 10) + ")")
-            .style("width", "95")
-            .style("height", "160px")
-            .style("stroke", "rgb(87, 114, 234)")
-            .style("stroke-width", "2")
-            .style("fill", "white")
-            .style("fill-opacity", "0.0001")
-        ;
+            .attr("transform", rectTransfrom)
+            .style("width", rectWidth)
+            .style("height", rectHeight)
+            .style("stroke", rectBorderColor)
+            .style("stroke-width", rectBorderWidth)
+            .style("fill-opacity", rectOpacity);
 
     var legend = parent_svg
         .append("g")
         .attr("id", "legend")
-        .attr("transform", "translate(" + (margin.left + width - 40) + "," + (margin.top) + ")")
+        .attr("transform", legendTransfrom)
         .append("rect")
         .attr("width", legendWidth)
         .attr("height", legendHeight)
-        .style("fill", "url(#linear-gradient)")
+        .style("fill", "url(#linear-gradient)");
 
     var legendGroupTag = parent_svg.select("#legend");
     legendGroupTag.append('text')
         .attr('x', -24)
         .attr('y', 12)
-        .style("font-family", "Times New Roman")
+        .style("font-family", fontFamily)
         .text(legendLabels[0]);
+
     legendGroupTag.append('text')
         .attr('x', -37)
         .attr('y', legendHeight - 3)
-        .style("font-family", "Times New Roman")
+        .style("font-family", fontFamily)
         .text(legendLabels[1]);
 
     var defs = parent_svg.append("defs");
